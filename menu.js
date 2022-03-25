10ab612a6673b1c5fc52020da91178630aefb80d
tirthadas9775 <75427198+tirthadas9775@users.noreply.github.com>
2022-03-25 12:45:13 +0600

    navbar fixed

--- menu.js
+++ menu.js
@@ -9,4 +9,12 @@ $(document).ready(function() {
     closeBtn.click(function() {
         sideMenu.removeClass('active');
     });
+    $(window).bind('scroll', function() {
+        var menu = $('.initial-landing');
+        if ($(window).scrollTop()) {
+            menu.addClass('fixed');
+        } else {
+            menu.removeClass('fixed');
+        }
+    });
 });
